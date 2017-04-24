7a5606b83b08a7d702518ed138ef291191228ce6
Aaron <Aaron@DESKTOP-D5077KT.attlocal.net>
2017-04-23 21:03:48 -0500

    Finished Labs 1 through 4.

---
+++ src/common/Dog.java
@@ -0,0 +1,66 @@
+package common;
+
+public class Dog {
+    private String name;
+    private int rabiesId;
+
+    public Dog() {
+    }
+
+    public Dog(String name, int rabiesId) {
+        this.name = name;
+        this.rabiesId = rabiesId;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRabiesId() {
+        return rabiesId;
+    }
+
+    public void setRabiesId(int rabiesId) {
+        this.rabiesId = rabiesId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + this.rabiesId;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Dog other = (Dog) obj;
+        if (this.rabiesId != other.rabiesId) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog {" + "Name: " + name + ", Rabies Id: " + rabiesId + "}";
+    }
+    
+    
+
+    
+
+}
---
+++ src/common/Startup.java
@@ -0,0 +1,87 @@
+package common;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ *
+ * @author agnas
+ */
+public class Startup {
+
+    public static void main(String[] args) {
+
+        //Lab 2
+        List employee = new ArrayList();
+
+        employee.add("Bob Jones");
+        employee.add("Sal Smith");
+        employee.add("Susan Jackson");
+
+        System.out.println("List of Employees not using generics:");
+
+        for (int i = 0; i < employee.size(); i++) {
+            String employeeName = (String) employee.get(i);
+            System.out.println(employeeName);
+        }
+
+        System.out.println("\nList of Employees not using generics with an enhanced for loop:");
+
+        for (Object employeeName : employee) {
+            System.out.println((String) employeeName);
+        }
+
+        //Lab 3
+        List<String> employeeTwo = new ArrayList<String>();
+
+        employeeTwo.add("Linda Van");
+        employeeTwo.add("Carl Johnson");
+        employeeTwo.add("Susan Blue");
+
+        System.out.println("\nList of Employees using generics:");
+
+        for (int i = 0; i < employeeTwo.size(); i++) {
+            String employeeName = employeeTwo.get(i);
+            System.out.println(employeeName);
+        }
+
+        System.out.println("\nList of Employees using generics and enhanced for loop:");
+
+        //this is like saying iterate through all of the items in the employeeTwo array and
+        //print them out as employeeName strings
+        for (String employeeName : employeeTwo) {
+            System.out.println(employeeName);
+        }
+
+        //Lab 4 (rules of equality based on rabies ID)
+        Dog dogOne = new Dog("Sparky", 001);
+        Dog dogTwo = new Dog("Rex", 002);
+        Dog dogThree = new Dog("Speedy", 001);
+        Dog dogFour = new Dog("Champ", 004);
+
+        List<Dog> dogList = new ArrayList<Dog>();
+        dogList.add(dogOne);
+        dogList.add(dogTwo);
+        dogList.add(dogThree);
+        dogList.add(dogFour);
+
+        //to show the duplicate
+        System.out.println("\nThis is a list of the dogs with the duplicates: ");
+        for (Dog dogInList : dogList) {
+            System.out.println(dogInList);
+        }
+
+        //use the set to remove the duplicates
+        Set<Dog> dogSet = new LinkedHashSet<Dog>(dogList);
+
+        System.out.println("\nThis is using the set. It removes the duplicates: ");
+
+        for (Dog dogInSet : dogSet) {
+            System.out.println(dogInSet);
+        }
+    }
+
+}
